692646b3cb68b0ecbb72bfef6f218281b3e841f9
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-10-27 12:15:46 +0300

    updates

--- backEnd/controllers/deleteHotel.js
+++ backEnd/controllers/deleteHotel.js
@@ -1,8 +1,8 @@
 import hotel from "../models/hotel.js";
 const deleteHotel =async (req,res)=>{
     try {
-         const deletedHotel= await hotel.findByIdAndDelete(req.params.id)
-        res.status(200).json(deletedHotel)
+         const deletedHotel= await hotel.findByIdAndDelete(req.params.hotelId)
+        res.status(200).json("hotel has been delete")
     } catch (error) {
         res.status(500).json(error)
     }
--- backEnd/controllers/updateHotel.js
+++ backEnd/controllers/updateHotel.js
@@ -1,7 +1,7 @@
 import hotel from "../models/hotel.js"
 const updateHotel= async (req,res)=>{
       try {
-        const updated= await hotel.findByIdAndUpdate(req.params.hotelId,{$set:req.body})
+        const updated= await hotel.findByIdAndUpdate(req.params.hotelId,{$set:req.body},{new:true})
         res.status(200).json(updated);
       } catch (err) {
         res.status(500).json(err)
