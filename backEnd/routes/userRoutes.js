d0dd9add483ff3016eeb70cf3e88bbce536daf1c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2022-11-01 10:44:50 +0300

    feat:verifying user & is admin

--- backEnd/routes/userRoutes.js
+++ backEnd/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import {getAllUsers,getUser,updateUser,deleteUser} from "../users/userController.js";
 import express from "express";
-import { verifyToken } from "../util/verifyToken.js";
+import { verifyToken,verifyUser } from "../util/verifyToken.js";
 const userRouter = express.Router();
 userRouter.route('/:dataAmount').get(getAllUsers)
 userRouter.route("/:userId").get(getUser).patch(updateUser).delete(deleteUser);
 
-userRouter.route("//check/checkAuthentication/").get(verifyToken,(req,res,next)=>{
+userRouter.route("/check/checkAuthentication").get(verifyToken,(req,res,next)=>{
     res.send( "you are authenticated");
+})
+.get("/user/:id",verifyUser,(req,res)=>{
+    res.send("you are logged in and you can delete")
 })
 export default userRouter;
